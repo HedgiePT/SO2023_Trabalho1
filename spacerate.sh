8913404c9b20ff901043173c816e7bbc19db6c9f
João Diogo Craveiro Ferreira <devilj@outlook.pt>
2023-11-12 20:59:14 +0000

    spacerate.sh: Já funciona praticamente tudo!

--- spacerate.sh
+++ spacerate.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+###############################################################################
+# FIXME!    FIXME!    FIXME!    FIXME!    FIXME!    FIXME!    FIXME!    FIXME!#
+#                                                                             #
+# * Não faço ideia do que acontece quando um diretório tem tamanho "NA".      #
+#                                                                             #
+###############################################################################
+
 ################################
 # Escrever mensagens de ajuda ##
 ################################
 
 function help_usage()
 {
-    echo "Utilização: $0 [parâmetros] relatório1 relatório2" >&2
-}
-
-
-function debug
-{
-    echo "$1" >&2
+    echo "Utilização: $0 [parâmetros] relatório_novo relatório_velho" >&2
 }
 
 ####################
@@ -47,7 +48,7 @@ SORT_COLUMN_FILE_NAME=2
 SORT_COLUMN_DEFAULT=$SORT_COLUMN_FILE_SIZE
 
 # Variáveis
-out_sort_mode=$SORT_COLUMN_DEFAULT
+out_sort_by_name=0
 out_sort_invert=0
 out_max_lines=-1
 
@@ -55,7 +56,7 @@ out_max_lines=-1
 # FIXME: Detetar argumentos inválidos.
 while getopts "arl:" optparam; do
     case $optparam in
-        a ) out_sort_mode=$SORT_COLUMN_FILE_NAME ;;
+        a ) out_sort_by_name=1 ;;
         r ) out_sort_invert=1 ;;
         l ) out_max_lines=${OPTARG} ;;
         ? ) bad_parameter ;;
@@ -68,31 +69,93 @@ for i in "$@"
     echo "DEBUG: PARAM: $i" >&2
 }
 
+######################################################################
+# Carregar relatórios para arrays associativos (dicionários)  ########
+######################################################################
 
-function process_reports
-# ARGUMENTOS:
-#   - $1: relatório mais recente;
-#   - $2: relatório mais antigo.
+report_new=${@:$OPTIND:1}
+report_old=${@:$OPTIND+1:1}
+
+declare -A new
+declare -A old
+
+echo "A carregar o relatório novo..." >&2
+while read -r line; do
+    size=$(cut -d ' ' -f 1 <<<$line)
+    dir=$(cut -d ' ' -f 2- <<<$line)
+
+    new[$dir]=$size
+done < <(tail -n +2 $report_new)
+
+echo "A carregar o relatório velho..." >&2
+while read -r line; do
+    size=$(echo $line | cut -d ' ' -f 1)
+    dir=$(echo $line | cut -d ' ' -f 2-)
+
+    old[$dir]=$size
+done < <(tail -n +2 $report_old)
+
+#echo "DEBUG: new:"$'\n'"${!new[@]}" >&2
+#echo "DEBUG: old:"$'\n'"${!old[@]}" >&2
+
+#################################################
+# Comparar dicionários e processar diferenças   #
+#################################################
+
+declare -A where
+declare diff=""
+
+echo "A construir lista de diretórios presentes no relatório novo..." >&2
+for dir in "${!new[@]}"
 {
-    report_new=$1
-    report_old=$2
-    
-    declare -A new
-    declare -A old
-    
-    while read -r line; do
-        size=$(echo $line | cut -d ' ' -f 1)
-        dir=$(echo $line | cut -d ' ' -f 2-)
-        
-        new[$dir]=$size
-    done < <(tail -n +2 $1)
-    
-    while read -r line; do
-        size=$(echo $line | cut -d ' ' -f 1)
-        dir=$(echo $line | cut -d ' ' -f 2-)
-        
-        old[$dir]=$size
-    done < <(tail -n +2 $2)
+    where[$dir]=$((1))
 }
 
-process_reports $1 $2
+echo "A construir lista de diretórios presentes no relatório velho..." >&2
+for dir in "${!old[@]}"
+{
+    where[$dir]=$((where[$dir] + 2))
+}
+
+echo "A comparar tamanhos..." >&2
+for dir in "${!where[@]}"
+{
+    #echo "DEBUG: where:"$'\n'"DIR | VALUE"$'\n'"$dir | ${where[$dir]}" >&2
+    #echo "new=${new[$dir]}; old=${old[$dir]}" >&2
+    size=$((new[$dir] - old[$dir]))
+    #echo -e "$size\n" >&2
+    
+    if ((where[$dir] == 3)); then   # Diretório existe em ambos os relatórios.
+        suffix=$''
+    elif ((where[$dir] == 2)); then  # Dir existe só no relatório velho.
+        suffix=$' REMOVED'
+    else    # Diretório existe apenas no relatório mais recente.
+        suffix=$' NEW';
+    fi
+
+    IFS=$'\n' diff=("${diff[*]}""$size $dir$suffix"$'\n')
+}
+
+echo "A ordenar resultados..." >&2
+declare -a sort_options=()
+
+if ((out_sort_by_name)); then
+    sort_options+=($'-k2')
+else
+    sort_options+=($'-nk1,1')
+fi
+
+if ((out_sort_invert == out_sort_by_name)); then
+    sort_options+=($'-r')
+fi
+
+#   echo "DEBUG: sort_options: ${sort_options[@]}"
+IFS=$'\n' diff=$(sort ${sort_options[*]} <<<${diff[*]})
+
+if ((out_max_lines > -1)); then
+    diff=$(head -n $out_max_lines <<<${diff[*]})
+fi
+
+echo "A imprimir resultados..." >&2
+echo "SIZE NAME"
+IFS='\n' cat <<<${diff[*]}
